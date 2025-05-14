c7b374d92b518dd6a7ca8c0dbef82058961ce0d4
Matheus Lacerda <matheus.l1996@gmail.com>
2025-05-14 14:42:22 -0300

    swagger, gemini and ollama

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('News Hook TTS API')
+    .setDescription('The News Hook TTS API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = configService.get<number>('PORT', 3000);
   await app.listen(port);
 }
