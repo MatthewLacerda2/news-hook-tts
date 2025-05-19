53fbb1063a8af00cbab753a6196f9123c87e3763
Matheus Lacerda <matheus.l1996@gmail.com>
2025-05-19 17:00:44 -0300

    initial migration

---
+++ src/migrations/1747684809578-CreateAlertEvents.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateAlertEvents1747684809578 implements MigrationInterface {
+    name = 'CreateAlertEvents1747684809578'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "alert_events" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "payload" jsonb NOT NULL, CONSTRAINT "PK_f8dd833a0534d3a01e8d01e3bca" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "alerts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "prompt" text NOT NULL, "http_method" character varying NOT NULL, "http_url" character varying NOT NULL, "http_headers" jsonb, "llm_model" character varying, "is_recurring" boolean NOT NULL, "payload_format" jsonb, "max_datetime" TIMESTAMP, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_60f895662df096bfcdfab7f4b96" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "alerts"`);
+        await queryRunner.query(`DROP TABLE "alert_events"`);
+    }
+
+}
