16c6a03a6c1fae2243a44927af6fd6309ede9abd
Matthew Lacerda <math2.0@hotmail.com>
2025-05-19 14:22:30 -0300

    client-sdk v1 (#1)
    Co-authored-by: Matheus Lacerda <matheus.l1996@gmail.com>

---
+++ src/client-sdk/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AlertsApi';
+export * from './AuthApi';
+export * from './DefaultApi';
+export * from './LlmModelsApi';
+export * from './UserDocumentsApi';
---
+++ src/client-sdk/models/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentControllerResponse';
+export * from './AlertPromptCreateRequestBase';
+export * from './AlertPromptCreateSuccessResponse';
+export * from './AlertPromptItem';
+export * from './AlertPromptListResponse';
+export * from './AlertStatus';
+export * from './HTTPValidationError';
+export * from './HttpMethod';
+export * from './LLMModelItem';
+export * from './LLMModelListResponse';
+export * from './OAuth2Request';
+export * from './TokenResponse';
+export * from './UserDocumentCreateRequest';
+export * from './UserDocumentCreateSuccessResponse';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
