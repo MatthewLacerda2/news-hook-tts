0b35586cf5e201259e12bf0d3a0bcc3d844ce870
Matheus Lacerda <matheus.l1996@gmail.com>
2025-06-04 15:10:12 -0300

    alert events endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 import httpx
 import models
@@ -12,7 +12,7 @@ app = FastAPI(title="News Hook TTS")
 
 ALERTS_API_URL = "http://127.0.0.1:8000/api/v1/"
 
-@app.post("/create-alert", response_model=dict)
+@app.post("/create-alert", response_model=dict, status_code=status.HTTP_201_CREATED)
 async def create_alert(
     request: schemas.AlertPromptCreateRequest,
     db: Session = Depends(get_db)
@@ -46,3 +46,46 @@ async def create_alert(
                 status_code=500,
                 detail=f"Failed to forward alert to alerts service: {str(e)}"
             )
+
+@app.post("/generic-alert", response_model=dict, status_code=status.HTTP_201_CREATED)
+def create_generic_alert(
+    request: schemas.GenericAlertCreateRequest,
+    db: Session = Depends(get_db)
+):
+    # Create the generic alert in our database
+    db_alert = models.GenericAlert(
+        content=request.content
+    )
+    db.add(db_alert)
+    db.commit()
+    db.refresh(db_alert)
+    
+    return {
+        "id": db_alert.id,
+        "created_at": db_alert.created_at,
+        "content": db_alert.content
+    }
+
+@app.post("/news-alert", response_model=dict, status_code=status.HTTP_201_CREATED)
+def create_news_alert(
+    request: schemas.NewsAlertCreateRequest,
+    db: Session = Depends(get_db)
+):
+    # Create the news alert in our database
+    db_alert = models.NewsAlert(
+        title=request.title,
+        content=request.content,
+        keywords=request.keywords,
+        entities=request.entities,
+        due_date=request.due_date,
+        source_url = request.source_url
+    )
+    db.add(db_alert)
+    db.commit()
+    db.refresh(db_alert)
+
+    return {
+        "id": db_alert.id,
+        "created_at": db_alert.created_at,
+        "title": db_alert.title,
+    }
